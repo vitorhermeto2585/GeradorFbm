dc28b2cd305536ebb0e603bc0adc3f587cf0c1b8
vitorhermeto2585 <d2019003557@unifei.edu.br>
2024-12-08 00:56:24 -0300

    Update __init__.py

--- geradorfbm/__init__.py
+++ geradorfbm/__init__.py
@@ -4,7 +4,8 @@
 __version__ = "0.1.0"
 
 # Importa as funções principais do pacote
-from .funcoes import DFA, FBM
+from .funcoes import DFA
+from .funcoes import FBM
 
 # Lista de elementos disponíveis ao importar o pacote
 __all__ = ["DFA", "FBM"]
